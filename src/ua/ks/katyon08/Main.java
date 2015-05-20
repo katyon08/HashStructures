3963a0715da93be5ea63596f6d1e90764b25e79c
katyon08 <katyon08@gmail.com>
2015-05-20 19:45:11 +0400

    1.2.2

--- src/ua/ks/katyon08/Main.java
+++ src/ua/ks/katyon08/Main.java
@@ -1,6 +1,7 @@
 package ua.ks.katyon08;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.Random;
 
@@ -8,6 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
         HashTable<Integer, Integer> table = new HashTable<Integer, Integer>();
+        HashSet
         Random rnd = new Random();
         int x;
         for (int i = 0; i < 20; i++) {
